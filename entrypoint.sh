7aebf3cf74b17ceae8434427e61ed5a8b61b631e
yellow0519 <70816562+yellow0519@users.noreply.github.com>
2022-08-10 14:15:45 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 # Either use the uid and gid if passed in at runtime or
 # fallback to 100
 
-USER_ID=${uid:-100}
-GROUP_ID=${gid:-100}
+USER_ID=${uid:-9001}
+GROUP_ID=${gid:-9001}
 
 #addgroup --gid $GROUP_ID mygroup
 #adduser --disabled-password myuser --uid $USER_ID --gecos myuser --ingroup mygroup --shell /bin/sh
