9dea1b8c71e2f2560ab87a0453bcb79b852aa43b
yellow0519 <70816562+yellow0519@users.noreply.github.com>
2022-08-10 14:39:38 +0900

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -3,7 +3,7 @@
 # For more information visit: https://github.com/downthecrop/TwitchVOD
 
 while [ true ]; do
-	Date=$(date +%Y%m%d-%H%M%S)
+	Date=$(date +%Y년%m월%d일-%H%M)
 	streamlink $streamOptions $streamLink best -o /home/download/{title}"-$Date".mkv
 	sleep 60s
 done
